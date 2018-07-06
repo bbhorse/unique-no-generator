6687570593bf3d9eae8b4f19ef1652a71934a4e1
yubo <yubo@bbdservice.com>
2018-07-06 10:49:24 +0800

    DB script.

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `sequence_key`;
+CREATE TABLE `sequence_key` (
+  `id` int(11) NOT NULL,
+  `key` varchar(25) NOT NULL,
+  `start_with` bigint(20) DEFAULT '1',
+  `inc_span` int(11) DEFAULT '1',
+  `description` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_sequence_key` (`key`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
